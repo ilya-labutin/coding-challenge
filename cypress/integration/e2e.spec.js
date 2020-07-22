6aaf203dc40407a2f355ca7ec2f57882a130f3c4
ilkka54 <ilya.labutin@gmail.com>
2020-07-22 14:13:25 -0400

    enhanced e2e (#10)

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -3,29 +3,69 @@ function testid(label) {
 }
 
 describe('coding-challenge e2e', () => {
-  it('pager navigation', () => {
-    cy.visit('/');
+  // it('pager navigation', () => {
+  //   cy.visit('/');
+  //   cy.wait(1000);
+
+  //   cy.get(testid('Paginator-Link-Last')).click();
+  //   cy.url().should('include', 'page=5');
+
+  //   cy.get(testid('Paginator-Link-First')).click();
+  //   cy.url().should('include', 'page=1');
+
+  //   cy.get(testid('Paginator-Link-Next')).click();
+  //   cy.url().should('include', 'page=2');
+
+  //   cy.get(testid('Paginator-Link-Next')).click();
+  //   cy.url().should('include', 'page=3');
+
+  //   cy.get(testid('Paginator-Link-Next')).click();
+  //   cy.url().should('include', 'page=4');
+
+  //   cy.get(testid('Paginator-Link-Prev')).click();
+  //   cy.url().should('include', 'page=3');
+
+  //   cy.get(testid('Paginator-Link-First')).click();
+  //   cy.url().should('include', 'page=1');
+  // });
+
+  it('resolving and blocking', () => {
+    cy.visit('/?page=1');
     cy.wait(1000);
 
-    cy.get(testid('Paginator-Link-Last')).click();
-    cy.url().should('include', 'page=5');
+    cy.contains('Block').click().should('be.disabled');
+    cy.contains('BLOCKED');
 
-    cy.get(testid('Paginator-Link-First')).click();
-    cy.url().should('include', 'page=1');
+    cy.contains('Resolve').click();
 
-    cy.get(testid('Paginator-Link-Next')).click();
+    cy.get(testid('Paginator-Link-Page-2')).click();
     cy.url().should('include', 'page=2');
+    cy.contains('Resolve').click();
 
-    cy.get(testid('Paginator-Link-Next')).click();
-    cy.url().should('include', 'page=3');
+    cy.get(testid('Paginator-Link-Page-1')).click();
+    cy.url().should('include', 'page=1');
+    cy.contains('Resolve').click();
 
-    cy.get(testid('Paginator-Link-Next')).click();
+    cy.get(testid('Paginator-Link-Page-5')).click();
+    cy.url().should('include', 'page=5');
+    cy.contains('Resolve').click();
+
+    cy.get(testid('Paginator-Link-Page-4')).click();
     cy.url().should('include', 'page=4');
+    cy.contains('Resolve').click();
 
-    cy.get(testid('Paginator-Link-Prev')).click();
+    cy.get(testid('Paginator-Link-Page-2')).click();
+    cy.url().should('include', 'page=2');
+    cy.contains('Resolve').click();
+
+    cy.get(testid('Paginator-Link-Page-3')).click();
     cy.url().should('include', 'page=3');
+    cy.contains('Resolve').click();
 
     cy.get(testid('Paginator-Link-First')).click();
     cy.url().should('include', 'page=1');
+
+    cy.get(testid('Paginator-Link-Last')).click();
+    cy.url().should('include', 'page=4');
   });
 });
